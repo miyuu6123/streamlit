39bdbbcbf560c5ea3dad9aa8713b8a0304eb51a0
miyuu6123 <157795023+miyuu6123@users.noreply.github.com>
2024-11-28 10:44:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
-# streamlit
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import streamlit as st
+import numpy as np
+import pandas as pd
 
-st.write('これは公開用のstreamlitアプリです。')
+st.title("Web App")
+st.write("**これがstreamlitアプリ**")
+
+answer = st.button('Say hello')
+
+if answer == True:
+     st.write('Why hello there')
+else:
+     st.write('Goodbye')
+    
+data = {
+    'lat': np.random.randn(100) / 100 + 35.68,
+    'lon': np.random.randn(100) / 100 + 139.75,
+}
+map_data = pd.DataFrame(data)
+# 地図に散布図を描く
+st.map(map_data)
